195ad28f5100bd987341b740f8ed8a86869d6be8
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-08 17:49:51 -0500

    C:/Program Files/Git/profile/id and /image

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
 	const {email, name, password } = req.body;
 	database.users.push({
-		id: '124',
+		id: '126',
 		name: name,
 		email: email,
 		password: password,
@@ -49,6 +49,35 @@ app.post('/register', (req, res) => {
 		joined: new Date()
 	})
 	res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+	const {id} = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id) {
+			found =true;
+			return res.json(user);
+		} 
+	})
+		if(!found) {
+			res.status(400).json('not found');
+		}
+	})
+
+app.post('/image', (req,res) => {
+	const {id} = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id) {
+			found =true;
+			user.entries++;
+			return res.json(user.entries);
+		} 
+})
+	if(!found) {
+		res.status(400).json('not found');
+			}
 })
 
 app.listen(3000, () => {
