23d5098333fedac1c6dd37c4c0f2ee92a5833207
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-09 14:07:42 -0500

    storing user passwords

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -10,7 +11,6 @@ const database = {
 		id: '123',
 		name: 'John',
 		email:'john@gmail.com',
-		password:'cookies',
 		entries: 0,
 		joined: new Date()
 	},
@@ -18,11 +18,17 @@ const database = {
 		id: '124',
 		name: 'Sally',
 		email:'sally@gmail.com',
-		password:'bananas',
 		entries: 0,
 		joined: new Date()
-	},
-	]
+	}
+	],
+	/*login: [
+		{
+			id: '987'
+			hash: '',
+			email: 'john@gmail.com'	
+		}
+	]*/
 }
 
 app.get('/', (req, res) => {
@@ -30,6 +36,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+	bcrypt.compare("apples", '$2a$10$uThAn1mKOUJbfy8gUuO0WemFSKVFCUjx.WhRgwMar6tAYMLA1QpoS', function(err, res) {
+    console.log('first guess', res);
+	});
+	bcrypt.compare("veggies", '$2a$10$uThAn1mKOUJbfy8gUuO0WemFSKVFCUjx.WhRgwMar6tAYMLA1QpoS', function(err, res) {
+	console.log('second guess', res);
+	});
 	if (req.body.email === database.users[0].email && 
 			req.body.password === database.users[0].password){
 		res.json('success');
@@ -40,7 +52,7 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const {email, name, password } = req.body;
-	database.users.push({
+		database.users.push({
 		id: '126',
 		name: name,
 		email: email,
@@ -83,3 +95,7 @@ app.post('/image', (req,res) => {
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
 })
+
+
+
+// Load hash from your password DB.
