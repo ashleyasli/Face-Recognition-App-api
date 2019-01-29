d4d4d7b492006d92bf9845b8f740cd95f43d37e3
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-29 13:40:34 -0500

    security review, move to Clarifai API to the back-end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+//You must add your API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: '2e5d4e7a2139464788c68f1af296e3b0'
+});
+
+const handleApiCall = (req, res ) => {
+	app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage =(req, res, db) => {
 	const {id} = req.body;
 	db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage =(req, res, db) => {
   }
 
 module.exports = {
-	handleImage:handleImage
+	handleImage, 
+	handleApiCall
 }
